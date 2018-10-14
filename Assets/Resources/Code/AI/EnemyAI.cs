73e271ea8323b168898f61db6ec666c0d2db7600
TheShroom <bajskorvspel@hotmail.com>
2018-10-14 20:24:41 +0200

    Added simple enemy AI.

---
+++ Assets/Resources/Code/AI/EnemyAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+    NavMeshAgent navAgent;
+    GameObject playerObject;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        playerObject = GameObject.FindWithTag("Player");
+    }
+
+}
