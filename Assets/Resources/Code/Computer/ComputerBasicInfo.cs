7e5b66d41e2bee3e9a2b7160561f945aa365c6fd
Alex <bajskorvspel@hotmail.com>
2018-10-15 21:07:29 +0200

    Added simple player interaction with objects and a simple computer system

---
+++ Assets/Resources/Code/Computer/ComputerBasicInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ComputerBasicInformation", menuName = "Computer/Basic Information")]
+public class ComputerBasicInfo : ScriptableObject {
+
+    public string operatingSystem;
+
+}
